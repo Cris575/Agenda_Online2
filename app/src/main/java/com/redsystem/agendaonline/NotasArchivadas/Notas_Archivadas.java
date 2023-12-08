ae6a3fd4cd8f79854f2f08d497f2d3d902cd1ad3
Cris <cg691539@gmail.com>
2023-12-07 18:13:01 -0600

    Initial commit

---
+++ app/src/main/java/com/redsystem/agendaonline/NotasArchivadas/Notas_Archivadas.java
@@ -0,0 +1,16 @@
+package com.redsystem.agendaonline.NotasArchivadas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.redsystem.agendaonline.R;
+
+public class Notas_Archivadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas_archivadas);
+    }
+}
---
+++ app/src/main/java/com/redsystem/agendaonline/Perfil/Perfil_Usuario.java
@@ -0,0 +1,16 @@
+package com.redsystem.agendaonline.Perfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.redsystem.agendaonline.R;
+
+public class Perfil_Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+    }
+}
